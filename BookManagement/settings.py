65c5b0cb1f5ed9fb7336da27df55cf621708369d
Kali <manishpc@localhost>
2025-10-11 00:19:04 +0530

    all site

--- BookManagement/settings.py
+++ BookManagement/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = os.getenv("DJANGO_SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG =  os.getenv("DJANGO_DEBUG")
 
-ALLOWED_HOSTS = [*]
+ALLOWED_HOSTS = ['book-management-system-90ro.onrender.com']
 
 
 # Application definition
