a215d338d0d07d42f1591ddf409b7eab37a49310
Kali <manishpc@localhost>
2025-10-11 04:45:21 +0530

    all db add

--- BookManagement/db_routers.py
+++ BookManagement/db_routers.py
@@ -1,27 +1,57 @@
 # myproject/routers.py
-class DbRouter:
-    """
-    Route 'books' and auth-related models to MySQL.
-    """
+# class DbRouter:
+#     """
+#     Route 'books' and auth-related models to MySQL.
+#     """
+#
+#     apps_mysql = ['user','book', 'auth', 'contenttypes', 'sessions', 'admin']
+#
+#     def db_for_read(self, model, **hints):
+#         if model._meta.app_label in self.apps_mysql:
+#             return 'mysql_db'
+#         return None
+#
+#     def db_for_write(self, model, **hints):
+#         if model._meta.app_label in self.apps_mysql:
+#             return 'mysql_db'
+#         return None
+#
+#     def allow_relation(self, obj1, obj2, **hints):
+#         if obj1._meta.app_label in self.apps_mysql or obj2._meta.app_label in self.apps_mysql:
+#             return True
+#         return None
+#
+#     def allow_migrate(self, db, app_label, model_name=None, **hints):
+#         if app_label in self.apps_mysql:
+#             return db == 'mysql_db'
+#         return None
 
+
+import os
+
+class DbRouter:
     apps_mysql = ['user','book', 'auth', 'contenttypes', 'sessions', 'admin']
 
     def db_for_read(self, model, **hints):
         if model._meta.app_label in self.apps_mysql:
-            return 'mysql_db'
+            if os.getenv('USE_MYSQL') == '1':
+                return 'mysql_db'
+            return 'default'
         return None
 
     def db_for_write(self, model, **hints):
         if model._meta.app_label in self.apps_mysql:
-            return 'mysql_db'
+            if os.getenv('USE_MYSQL') == '1':
+                return 'mysql_db'
+            return 'default'
         return None
 
     def allow_relation(self, obj1, obj2, **hints):
-        if obj1._meta.app_label in self.apps_mysql or obj2._meta.app_label in self.apps_mysql:
-            return True
-        return None
+        return True
 
     def allow_migrate(self, db, app_label, model_name=None, **hints):
         if app_label in self.apps_mysql:
-            return db == 'mysql_db'
+            if os.getenv('USE_MYSQL') == '1':
+                return db == 'mysql_db'
+            return db == 'default'
         return None
